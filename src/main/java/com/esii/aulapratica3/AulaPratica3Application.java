61c7eca64c91ed5d8d5cda237da412e5207c5905
Guilherme Barboza Mendonça <guilhermebmendonca60@gmail.com>
2022-06-17 22:21:19 -0300

    Primeiro commit. Arquivos iniciais adicionados

---
+++ src/main/java/com/esii/aulapratica3/AulaPratica3Application.java
@@ -0,0 +1,13 @@
+package com.esii.aulapratica3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaPratica3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaPratica3Application.class, args);
+	}
+
+}
